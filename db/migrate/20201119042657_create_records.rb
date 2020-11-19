f4f3335e38944cc3584227106b715ccb88b30a73
ryuki29 <ryuki.061529@gmail.com>
2020-11-19 13:34:36 +0900

    add record.rb

---
+++ db/migrate/20201119042657_create_records.rb
@@ -0,0 +1,10 @@
+class CreateRecords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :records do |t|
+      t.string :name
+      t.string :text
+      t.text :image
+      t.timestamps
+    end
+  end
+end
